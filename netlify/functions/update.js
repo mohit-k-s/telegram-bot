d5b1fe1c595213705c04f0c0763b749beeb06742
mohit-kr-s <mohit9205990@gmail.com>
2023-03-08 22:15:19 +0530

    remove unwanted code

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -3,19 +3,6 @@ var exec = require('child_process').exec
 
 
 exports.handler = async (event) => {
-  let pv = ''
-  exec("which python3", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    pv = stdout ;
-    console.log(`stdout: ${stdout}`);
-});
   const { message } = JSON.parse(event.body);
   const chatid = message.chat.id;
   const command = message.text;
@@ -23,13 +10,13 @@ exports.handler = async (event) => {
   python.on('error' , (err) =>{
     console.log(err);
   })
-  python.stdout.on('data', function (data) {
-   console.log(data.toString('utf-8'));
-  });
+  // python.stdout.on('data', function (data) {
+  //  console.log(data.toString('utf-8'));
+  // });
 
   python.on('close', (code) => {
   console.log(`child process close all stdio with code ${code}`);
   });
 
-  return { statusCode: 200 , body : `Sent and pv = ${pv}  ` };
+  return { statusCode: 200 , body : `Sent` };
 };
