4f8a8a79de129f0e1f0f6dc4db43e9f54bf586b9
mohit-kr-s <mohit9205990@gmail.com>
2023-03-08 22:28:07 +0530

    try with python only

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -5,14 +5,10 @@ exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
   const chatid = message.chat.id;
   const command = message.text;
-  const python = spawn('/usr/bin/python3', ['netlify/functions/main.py', `${command}` , chatid , command]);
+  const python = spawn('/usr/bin/python', ['netlify/functions/main.py', `${command}` , chatid , command]);
   python.on('error' , (err) =>{
     console.log(err);
   })
-  // python.stdout.on('data', function (data) {
-  //  console.log(data.toString('utf-8'));
-  // });
-
   python.on('close', (code) => {
   console.log(`child process close all stdio with code ${code}`);
   });
