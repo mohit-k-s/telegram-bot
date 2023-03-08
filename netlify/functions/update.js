b5aaf44f40b15bd1e98b942c0faa1e342a264839
mohit-kr-s <mohit9205990@gmail.com>
2023-03-08 20:32:46 +0530

    f

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -4,14 +4,14 @@ exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
   const chatid = message.chat.id;
   const command = message.text;
-  const python = spawn('python3', ['netlify/functions/main.py', `${command}` , chatid , command]);
+  const python = spawn('python', ['netlify/functions/main.py', `${command}` , chatid , command]);
   python.on('error' , (err) =>{
     console.log(err);
   })
   python.stdout.on('data', function (data) {
    console.log(data.toString('utf-8'));
   });
-  
+
   python.on('close', (code) => {
   console.log(`child process close all stdio with code ${code}`);
   });
