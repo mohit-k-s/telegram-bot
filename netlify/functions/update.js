26a14b6c02650f5894d502958d44ec08275ca7aa
mohit-kr-s <mohit9205990@gmail.com>
2023-03-08 22:35:52 +0530

    ck

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
   const chatid = message.chat.id;
   const command = message.text;
-  const python = spawn('/usr/bin/python', ['netlify/functions/main.py', `${command}` , chatid , command]);
+  const python = spawn('python3', ['netlify/functions/main.py', `${command}` , chatid , command]);
   python.on('error' , (err) =>{
     console.log(err);
   })
